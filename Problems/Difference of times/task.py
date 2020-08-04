e9a50b4070fabe167c64653523ad76565d5e39ab
Sergey Gurman <sgurman753@gmail.com>
2020-08-04 13:17:07 -0400

    problem of the day

---
+++ Problems/Difference of times/task.py
@@ -0,0 +1,15 @@
+# put your python code here
+class Time:
+    def __init__(self):
+        self.hour = int(input())
+        self.minutes = int(input())
+        self.seconds = int(input())
+        self.total_seconds = (self.hour*60*60) + (self.minutes*60) + self.seconds
+
+    def difference(self, time):
+        return abs(self.total_seconds - time.total_seconds)
+
+
+a = Time()
+b = Time()
+print(a.difference(b))
