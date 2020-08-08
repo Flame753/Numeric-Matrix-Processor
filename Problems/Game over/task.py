b3bc3af6b218479971b9086833beea6463d92cef
Sergey Gurman <sgurman753@gmail.com>
2020-08-08 00:01:08 -0400

    update

--- Problems/Game over/task.py
+++ Problems/Game over/task.py
@@ -3,10 +3,9 @@ C_count = 0
 I_count = 0
 
 for ans in scores:
-    if ans == "C":
-        C_count += 1
-    if ans == "I":
-        I_count += 1
+    C_count += 1 if ans == "C" else 0
+    I_count += 1 if ans == "I" else 0
+
     if I_count == 3:
         print("Game over", C_count, sep="\n")
         break
