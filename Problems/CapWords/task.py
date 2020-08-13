dc0deecb9e3913f49dd080bd3d42b37d8cfa0562
Sergey Gurman <sgurman753@gmail.com>
2020-08-13 11:58:11 -0400

    problem of the day: Another way to do it

--- Problems/CapWords/task.py
+++ Problems/CapWords/task.py
@@ -1,5 +1,11 @@
+# My attempt
 phrase = str(input()).split('_')
 words = ''
 for word in phrase:
     words = words + word.capitalize()
 print(words)
+
+# Another way to do the same task
+phrase = input().split('_')
+for word in phrase:
+    print(word.capitalize(), end='')
