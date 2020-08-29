cd46225aca7bbb0bf09ddc4064bdc72760a753f9
Sergey Gurman <sgurman753@gmail.com>
2020-08-28 20:08:07 -0400

    problem of the day: update

--- Problems/Print book info/task.py
+++ Problems/Print book info/task.py
@@ -1,14 +1,8 @@
 def print_book_info(title, author=None, year=None):
-    t = f'"{title}"'
-    s = ' was written'
-    a = f' by {author}'
-    y = f' in {year}'
-
-    if author and year:
-        print(t + s + a + y)
-    elif author and not year:
-        print(t + s + a)
-    elif year and not author:
-        print(t + s + y)
-    else:
-        print(t)
+    print(f'"{title}"', end="")
+    if author or year:
+        print(" was written", end="")
+    if author:
+        print(f" by {author}", end="")
+    if year:
+        print(f" in {year}")
